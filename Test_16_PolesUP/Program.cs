f2898b04eff00da3d8e4b7de7c6df36d3c6640e0
37533 <37533@LAPTOP-GAM2DSJ2>
2023-05-20 23:27:06 +0300

    Добавьте файлы проекта.

---
+++ Test_16_PolesUP/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Test_16_PolesUP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                Console.WriteLine("Ведите число");
+                double number = Convert.ToInt32(Console.ReadLine());
+                int Count = Convert.ToInt32(Math.Log(number, 3));
+                
+                double X;
+                do
+                {
+                    X = Math.Pow(3, Count);
+                    number -= X;
+                    if (number < 0)
+                    {
+                        number += X;
+                    }
+                    Count--;
+
+                } while (number != 0 && Count > 0);
+
+                if (number == 0)
+                {
+                    Console.WriteLine(true);
+                }
+                else
+                {
+                    Console.WriteLine(false);
+                }
+            }
+        }
+    }
+}
